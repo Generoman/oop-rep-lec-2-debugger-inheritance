6ae25df66f7ffc5c8e235ddb34bb25f0b61fb6e8
Generoman <53780885+Generoman@users.noreply.github.com>
2022-03-15 20:22:13 +0100

    Debug part done

--- src/ClassNameIsNotImportant.java
+++ src/ClassNameIsNotImportant.java
@@ -2,5 +2,18 @@ public class ClassNameIsNotImportant {
 
     public static void main(String[] args) {
 
+        DebuggerPractice debugP = new DebuggerPractice();
+
+        debugP.setOurInt(10);
+        debugP.setOurString("Yo ho, yo ho, a pirate's life for me!");
+
+        System.out.println("\nOur int is now " + debugP.getOurInt());
+        System.out.println("Our string is now " + debugP.getOurString());
+
+        debugP.capitalizeOurString();
+        debugP.doubleOurInt();
+
+        System.out.println("\nOur int is now " + debugP.getOurInt());
+        System.out.println("Our string is now " + debugP.getOurString());
     }
 }
---
+++ src/DebuggerPractice.java
@@ -0,0 +1,57 @@
+import java.util.Locale;
+
+public class DebuggerPractice {
+
+    // FIELDS
+
+    private int ourInt;
+    private String ourString;
+
+
+
+    // GETTERS/SETTERS
+
+    public int getOurInt() {
+        return ourInt;
+    }
+
+    public void setOurInt(int ourInt) {
+        this.ourInt = ourInt;
+    }
+
+    public String getOurString() {
+        return ourString;
+    }
+
+    public void setOurString(String ourString) {
+        this.ourString = ourString;
+    }
+
+
+
+    // METHODS
+
+    // Denne metoden skal endre ourInt til å være dobbelt så stor
+    // Akkurat nå er denne koden feil i forhold til hva vi vil den skal gjøre
+    public int doubleOurInt() {
+        return ourInt * 2;
+    }
+
+    // Denne metoden skal endre ourString til å bare inneholde store bokstaver
+    // Akkurat nå er denne koden feil i forhold til hva vi vil den skal gjøre
+    public String capitalizeOurString() {
+        return ourString.toUpperCase();
+    }
+
+    // Riktig kode for begge metoder:
+    // Merk av all utkommentert kode, og trykk ctrl og /
+    // (cmd og 7 på mac)
+
+//    public void doubleOurInt() {
+//        ourInt = ourInt * 2;
+//    }
+
+//    public void capitalizeOurString() {
+//        ourString = ourString.toUpperCase();
+//    }
+}
